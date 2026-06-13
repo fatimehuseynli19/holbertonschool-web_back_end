ca706c945185fb02d436a5ce118d98e0da8913b2
fatimehuseynli19 <13703@holbertonstudents.com>
2026-06-13 19:58:52 +0000

    Fix line too long in 4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,5 +7,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn task_wait_random n times and return sorted delays."""
-    delays = await asyncio.gather(*[task_wait_random(max_delay) for _ in range(n)])
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
     return sorted(delays)
